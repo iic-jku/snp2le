6e4944a9140271b9ec58df98f7d6c525e58e5f8c
Simon Dorrer <simon.dorrer@jku.at>
2026-07-04 14:42:38 +0200

    bumped to version `0.1.2`

--- snp2le/__init__.py
+++ snp2le/__init__.py
@@ -4,4 +4,4 @@ A PySide6 GUI and command-line tool that turns a Touchstone .sNp file into an
 equivalent lumped-element netlist for Ngspice (SPICE3) and VACASK (Spectre),
 through a universal vector-fit macromodel or a structure-specific extractor.
 """
-__version__ = "0.1.1"
+__version__ = "0.1.2"
